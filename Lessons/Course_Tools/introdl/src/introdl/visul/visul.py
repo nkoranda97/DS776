25868c6c9358d8d7284463180254ea0174ccfd43
Jeff Baggett <baggett.jeff@gmail.com>
2025-02-12 13:25:01 -0600

    HW 4 Push

--- Lessons/Course_Tools/introdl/src/introdl/visul/visul.py
+++ Lessons/Course_Tools/introdl/src/introdl/visul/visul.py
@@ -323,19 +323,19 @@ def evaluate_classifier(model, dataset, device, display_confusion=True, img_size
 
     return confusion_mat, class_report, misclassified_dataset
 
-def visualize2DSoftmax(X, y, model, title=None):
+def visualize2DSoftmax(X, y, model, title=None, numpts=20):
     x_min = np.min(X[:,0])-0.5
     x_max = np.max(X[:,0])+0.5
     y_min = np.min(X[:,1])-0.5
     y_max = np.max(X[:,1])+0.5
-    xv, yv = np.meshgrid(np.linspace(x_min, x_max, num=20), np.linspace(y_min, y_max, num=20), indexing='ij')
+    xv, yv = np.meshgrid(np.linspace(x_min, x_max, num=numpts), np.linspace(y_min, y_max, num=numpts), indexing='ij')
     xy_v = np.hstack((xv.reshape(-1,1), yv.reshape(-1,1)))
     with torch.no_grad():
         logits = model(torch.tensor(xy_v, dtype=torch.float32))
         y_hat = F.softmax(logits, dim=1).numpy()
 
     plt.figure(figsize=(5,5))
-    cs = plt.contourf(xv, yv, y_hat[:,0].reshape(20,20), levels=np.linspace(0,1,num=20), cmap=plt.cm.RdYlBu)
+    cs = plt.contourf(xv, yv, y_hat[:,0].reshape(numpts,numpts), levels=np.linspace(0,1,num=20), cmap=plt.cm.RdYlBu)
     ax = plt.gca()
     sns.scatterplot(x=X[:,0], y=X[:,1], hue=y, style=y, ax=ax)
     if title is not None:
